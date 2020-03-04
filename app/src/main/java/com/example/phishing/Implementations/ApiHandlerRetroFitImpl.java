fb676fd162c24a07e9335cf0b0d79234efacc064
simeonirudhayaraj <simeon.j@zohocorp.com>
2020-03-04 13:30:34 +0530

    network connection established

--- app/src/main/java/com/example/phishing/Implementations/ApiHandlerRetroFitImpl.java
+++ app/src/main/java/com/example/phishing/Implementations/ApiHandlerRetroFitImpl.java
@@ -1,7 +1,14 @@
 package com.example.phishing.Implementations;
 
+import com.example.phishing.DataModels.ResponseModel;
 import com.example.phishing.Interfaces.ApiHandler;
+import com.example.phishing.Interfaces.ApiServices;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,7 +21,19 @@ public class ApiHandlerRetroFitImpl implements ApiHandler {
                 .baseUrl(this.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        ApiServices api = retrofit.create(ApiServices.class);
+        Call<List<ResponseModel>> call = api.verify(url);
+        call.enqueue(new Callback<List<ResponseModel>>() {
+            @Override
+            public void onResponse(Call<List<ResponseModel>> call, Response<List<ResponseModel>> response) {
 
+            }
+
+            @Override
+            public void onFailure(Call<List<ResponseModel>> call, Throwable t) {
+
+            }
+        });
 
         return null;
     }
--- app/src/main/java/com/example/phishing/Interfaces/ApiServices.java
+++ app/src/main/java/com/example/phishing/Interfaces/ApiServices.java
@@ -1,6 +1,8 @@
 package com.example.phishing.Interfaces;
-import java.util.List;
 import com.example.phishing.DataModels.ResponseModel;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
