f2700be1e3b6ba39238922d079a75d788ab8827d
simeonirudhayaraj <simeon.j@zohocorp.com>
2020-03-17 22:06:49 +0530

    server Connection done

---
+++ app/src/main/java/com/example/phishing/Constants/UrlConstants.java
@@ -0,0 +1,5 @@
+package com.example.phishing.Constants;
+
+public class UrlConstants {
+    public static String url = "https://0ef401f4.ngrok.io/api/";
+}
---
+++ app/src/main/java/com/example/phishing/Interfaces/ApiDelegate.java
@@ -0,0 +1,5 @@
+package com.example.phishing.Interfaces;
+
+public interface ApiDelegate {
+    public void processResult(int result, String url);
+}
